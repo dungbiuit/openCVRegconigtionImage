003d8ec6b2387f677045d14cc3e93df47f2d782c
dungbiuit <dungnguyen.bill@gmail.com>
2020-06-16 15:09:09 +0700

    Update lại file lấy dữ liệu :w

--- get_img_features_and_datas.py
+++ get_img_features_and_datas.py
@@ -1,184 +1,172 @@
-import cv2 as cv
-import numpy as np
+import csv
 import glob
+import numpy as np
+from cv2 import cv2
 
-# Lay nhieu hinh tu 1 folder
-day_path_jpg = glob.glob("sang/*.jpg")
-day_path_png = glob.glob("sang/*.png")
-night_path_jpg = glob.glob("toi/*.jpg")
-night_path_png = glob.glob("toi/*.png")
+DAY_IMG = []
 
-# Gom cac hinh ban ngay vao 1 list
-day_img = []
-for img in day_path_jpg:
-	n = cv.imread(img)
-	day_img.append(n)
-for img in day_path_png:
-	n = cv.imread(img)
-	day_img.append(n)
-
-# Gom cac hinh ban dem vao 1 list
-night_img = []
-for img in night_path_jpg:
-	n = cv.imread(img)
-	night_img.append(n)
-for img in night_path_png:
-	n = cv.imread(img)
-	night_img.append(n)
+NIGHT_IMG = []
 
 # List chua tat ca du lieu sau khi trich xuat
-datas = []
+# IMPORTANT
+DATAS = []
+# Lay nhieu hinh tu 1 folder
+DAY_PATH_JPG = glob.glob("sang/*.jpg")
+DAY_PATH_PNG = glob.glob("sang/*.png")
+NIGHT_PATH_JPG = glob.glob("toi/*.jpg")
+NIGHT_PATH_PNG = glob.glob("toi/*.png")
 
-# Vong lap trich xuat du lieu cua hinh ban ngay
-for src in day_img:
+HEADER_TITLE = ['','mean_hue', 'stddev_hue', 'mean_sat', 'stddev_sat', 'mean_val', 'stddev_val', 'classification']
+# GOM CAC HINH ban ngay vao 1 list
+def addImageListAfterAddImage(imagePath, ListToAdd):
+    for img in imagePath:
+        n = cv2.imread(img)
+        ListToAdd.append(n)
 
-	# Chuyen doi he mau tu RGB -> HSV
-	if src.shape[2] == 3:
-		hsv_img = cv.cvtColor(src, cv.COLOR_BGR2HSV)
-	else:
-		hsv_img = cv.cvtColor(src, cv.COLOR_BGRA2HSV)
+def appendDayImageToDAY_IMG():
+    addImageListAfterAddImage(DAY_PATH_JPG, DAY_IMG)
+    addImageListAfterAddImage(DAY_PATH_PNG, DAY_IMG)
 
-	# Tinh trung binh cong va do lech chuan cua 3 kenh H,S,V
-	mean, stddev = cv.meanStdDev(hsv_img)
-	mean = mean.flatten()
-	stddev = stddev.flatten()
+def appendNightImageToNIGHT_IMG():
+    addImageListAfterAddImage(NIGHT_PATH_JPG, NIGHT_IMG)
+    addImageListAfterAddImage(NIGHT_PATH_PNG, NIGHT_IMG)
 
-	# Chuyen so lieu thanh 3 muc du lieu: low, medium, high
-	mean_hue = ""
-	stddev_hue = ""
-	mean_sat = ""
-	stddev_sat = ""
-	mean_val = ""
-	stddev_val = ""
+def returnHSV_ImageFromNormalImage(src):
+    if src.shape[2] == 3:
+        hsv_img = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+    else:
+        hsv_img = cv2.cvtColor(src, cv2.COLOR_BGRA2HSV)
+    return hsv_img
 
-	# Trung binh cong cua kenh H
-	if mean[0] <= 45:
-		mean_hue = "l"
-	elif mean[0] <= 75:
-		mean_hue = "m"
-	else:
-		mean_hue = "h"
+def returnMeanAndStddevFromHSV_Image(hsv_img):
+    mean, stddev = cv2.meanStdDev(hsv_img)
+    mean = mean.flatten()
+    stddev = stddev.flatten()
+    return mean, stddev
 
-	# Trung binh cong cua kenh S
-	if mean[1] <= 65:
-		mean_sat = "l"
-	elif mean[1] <= 130:
-		mean_sat = "m"
-	else:
-		mean_sat = "h"
+def returnMeanHueFromMean(mean):
+    if mean <= 45:
+        mean_hue = "l_avg"
+    elif mean <= 75:
+        mean_hue = "m_avg"
+    else:
+        mean_hue = "h_avg"
+    return mean_hue
 
-	# Trung binh cong cua kenh V
-	if mean[2] <= 100:
-		mean_val = "l"
-	elif mean[2] <= 180:
-		mean_val = "m"
-	else:
-		mean_val = "h"
+def returnMeanSatFromMean(mean):
+    if mean <= 65:
+        mean_sat = "l_avg"
+    elif mean <= 130:
+        mean_sat = "m_avg"
+    else:
+        mean_sat = "h_avg"
+    return mean_sat
 
-	# Do lech chuan cua kenh H
-	if stddev[0] <= 20:
-		stddev_hue = "l"
-	elif stddev[0] <= 40:
-		stddev_hue = "m"
-	else:
-		stddev_hue = "h"
+def returnMeanValFromMean(mean):
+    if mean <= 100:
+        mean_val = "l_avg"
+    elif mean <= 180:
+        mean_val = "m_avg"
+    else:
+        mean_val = "h_avg"
+    return mean_val
 
-	# Do lech chuan cua kenh S
-	if stddev[1] <= 33:
-		stddev_sat = "l"
-	elif stddev[1] <= 67:
-		stddev_sat = "m"
-	else:
-		stddev_sat = "h"
+def returnStdDevHueFromStdDev(stddev):
+    if stddev <= 20:
+        stddev_hue = "l_dev"
+    elif stddev <= 40:
+        stddev_hue = "m_dev"
+    else:
+        stddev_hue = "h_dev"
+    return stddev_hue
+                                                    
+def returnStdDevSatFromStdDev(stddev):
+    if stddev <= 33:
+        stddev_sat = "l_dev"
+    elif stddev <= 67:
+        stddev_sat = "m_dev"
+    else:
+        stddev_sat = "h_dev"
+    return stddev_sat
 
-	# Do lech chuan cua kenh V
-	if stddev[2] <= 33:
-		stddev_val = "l"
-	elif stddev[2] <= 67:
-		stddev_val = "m"
-	else:
-		stddev_val = "h"
+def returnStdDevValFromStdDev(stddev):
+    if stddev <= 33:
+        stddev_val = "l_dev"
+    elif stddev <= 67:
+        stddev_val = "m_dev"
+    else:
+        stddev_val = "h_dev"
+    return stddev_val
 
-	# Luu du lieu vao list
-	img_data = [mean_hue,stddev_hue,mean_sat,stddev_sat,mean_val,stddev_val,"s"]
-	datas.append(img_data)
+def returnCalculateValueListOfEachImageFromImageList(imageList):
+    for src in imageList:
+        # Chuyen doi he mau tu RGB -> HSV
+        hsv_img = returnHSV_ImageFromNormalImage(src)
+        # Tinh trung binh cong va do lech chuan cua 3 kenh H,S,V
+        mean, stddev = returnMeanAndStddevFromHSV_Image(hsv_img)
+        # Chuyen so lieu thanh 3 muc du lieu: low, medium, high
+        # Trung binh cong cua kenh H
+        mean_hue = returnMeanHueFromMean(mean[0])
+        # Do lech chuan cua kenh H
+        stddev_hue = returnStdDevHueFromStdDev(stddev[0]) 
+        # Trung binh cong cua kenh S
+        mean_sat = returnMeanSatFromMean(mean[1]) 
+        # Do lech chuan cua kenh S
+        stddev_sat = returnStdDevSatFromStdDev(stddev[1]) 
+        # Trung binh cong cua kenh V
+        mean_val = returnMeanValFromMean(mean[2]) 
+        # Do lech chuan cua kenh V
+        stddev_val = returnStdDevValFromStdDev(stddev[2]) 
+        # Luu du lieu vao list
+        valueList = [mean_hue, stddev_hue, mean_sat, stddev_sat, mean_val, stddev_val, "s"]
+        return valueList
+def introduceSentences():
+    print("\t\t\tĐỒ ÁN CUỐI KÌ KHAI PHÁ DỮ LIỆU ĐA PHƯƠNG TIỆN VÀ THỰC HÀNH CHUYÊN SÂU")
+    print("\t\tSINH VIÊN: NGUYỄN TRÍ DŨNG - N16DCPT005 VÀ THẠCH HẢI ĐĂNG - N16DCPT007")
 
-# Vong lap trich xuat du lieu cua hinh ban dem
-for src in night_img:
+def functionalSentences():
+    print("1.Rút trích dữ liệu và dữ liệu ảnh sáng")
+    print("2.Rút trích dữ liệu và dữ liệu ảnh tối")
+    print("3.Rút trích dữ liệu và dữ liệu ảnh sáng và tối")
 
-	# Chuyen doi he mau tu RGB -> HSV
-	if src.shape[2] == 3:
-		hsv_img = cv.cvtColor(src, cv.COLOR_BGR2HSV)
-	else:
-		hsv_img = cv.cvtColor(src, cv.COLOR_BGRA2HSV)
+def showMenu():
+    introduceSentences()
+    functionalSentences()
 
-	# Tinh trung binh cong va do lech chuan cua 3 kenh H,S,V
-	mean, stddev = cv.meanStdDev(hsv_img)
-	mean = mean.flatten()
-	stddev = stddev.flatten()
+def returnSelection():
+    selection = int(input("Mời bạn nhập lựa chọn: "))
+    while selection < 1 or selection > 3:
+        functionalSentences()
+        selection = int(input("Sai lựa chọn mời bạn nhập lại: "))
+    return selection
 
-	# Chuyen so lieu thanh 3 muc du lieu: low, medium, high
-	mean_hue = ""
-	stddev_hue = ""
-	mean_sat = ""
-	stddev_sat = ""
-	mean_val = ""
-	stddev_val = ""
-
-	# Trung binh cong cua kenh H
-	if mean[0] <= 45:
-		mean_hue = "l"
-	elif mean[0] <= 75:
-		mean_hue = "m"
-	else:
-		mean_hue = "h"
-
-	# Trung binh cong cua kenh S
-	if mean[1] <= 65:
-		mean_sat = "l"
-	elif mean[1] <= 130:
-		mean_sat = "m"
-	else:
-		mean_sat = "h"
-
-	# Trung binh cong cua kenh V
-	if mean[2] <= 100:
-		mean_val = "l"
-	elif mean[2] <= 180:
-		mean_val = "m"
-	else:
-		mean_val = "h"
-
-	# Do lech chuan cua kenh H
-	if stddev[0] <= 20:
-		stddev_hue = "l"
-	elif stddev[0] <= 40:
-		stddev_hue = "m"
-	else:
-		stddev_hue = "h"
-
-	# Do lech chuan cua kenh S
-	if stddev[1] <= 33:
-		stddev_sat = "l"
-	elif stddev[1] <= 67:
-		stddev_sat = "m"
-	else:
-		stddev_sat = "h"
-
-	# Do lech chuan cua kenh V
-	if stddev[2] <= 33:
-		stddev_val = "l"
-	elif stddev[2] <= 67:
-		stddev_val = "m"
-	else:
-		stddev_val = "h"
-
-	# Luu du lieu vao list
-	img_data = [mean_hue,stddev_hue,mean_sat,stddev_sat,mean_val,stddev_val,"t"]
-	datas.append(img_data)
+def returnDataListBySelection(selection):
+    if selection == 1:
+        appendDayImageToDAY_IMG()
+        dataList = returnCalculateValueListOfEachImageFromImageList(DAY_IMG)
+    elif selection == 2:
+        appendNightImageToNIGHT_IMG()
+        dataList = returnCalculateValueListOfEachImageFromImageList(NIGHT_IMG)
+    else:
+        appendDayImageToDAY_IMG()
+        dataList = returnCalculateValueListOfEachImageFromImageList(DAY_IMG)
+        appendNightImageToNIGHT_IMG()
+        dataList += returnCalculateValueListOfEachImageFromImageList(NIGHT_IMG)
+    return dataList
 
 # Luu du lieu vao file datas.txt
-with open("datas.txt", 'w') as f:
-    for d in datas:
-        f.write(str(d) + '\n')
+def writeDataToCSVFile():
+    with open("datas.csv", 'w') as f:
+        headerRow = csv.writer(f)
+        headerRow.writerow(HEADER_TITLE)
+        for d in DATAS:
+            f.write(str(d) + '\n')
 
+def executeProgram():
+    showMenu()
+    selection = returnSelection()
+    dataList = returnDataListBySelection(selection)
+    DATAS.append(dataList)
+    writeDataToCSVFile()
+
+executeProgram()
